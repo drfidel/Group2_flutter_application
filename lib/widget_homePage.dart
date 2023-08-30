9bcc07036c7a990a7891384b0686af5801b58e9d
Akiyo Fidel <akiyofidel@gmail.com>
2023-08-30 14:41:37 +0300

    separated home page widget from main

---
+++ lib/widget_homePage.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  void _addCar() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "car list",
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _addCar,
+        tooltip: 'Add Car',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
