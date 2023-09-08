a1798e47df198d361031ed3afd8bd9c8459bd9b1
Akiyo Fidel <akiyofidel@gmail.com>
2023-09-08 15:57:04 +0300

    cleaned code errors

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:ui_demo/container_exercise.dart';
+
+import 'container_exercise.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
